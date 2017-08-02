90781f7e149c5f14ed74035e8dc6a52746aa84d0
Bishwo Gurung <bishwo.gurung3@gmail.com>
2017-08-02 10:38:10 -0400

    Apply environment variable properties

--- WiseMoney/src/main/java/com/wisemoney/util/HibernateUtil.java
+++ WiseMoney/src/main/java/com/wisemoney/util/HibernateUtil.java
@@ -1,21 +1,68 @@
 package com.wisemoney.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
+//public class HibernateUtil {
+//
+//	private static SessionFactory sessionFactory(String filename) {
+//		Configuration c = new Configuration().configure(filename);
+//		ServiceRegistry sr = new StandardServiceRegistryBuilder().applySettings(
+//				c.getProperties()).build();
+//		return c.buildSessionFactory(sr);
+//	}
+//
+//	public static Session getSession() {
+//		return sessionFactory("hibernate.cfg.xml").openSession();
+//	}
+//}
+
 public class HibernateUtil {
 
 	private static SessionFactory sessionFactory(String filename) {
-		Configuration c = new Configuration().configure(filename);
-		ServiceRegistry sr = new StandardServiceRegistryBuilder().applySettings(
-				c.getProperties()).build();
-		return c.buildSessionFactory(sr);
-	}
 
-	public static Session getSession() {
-		return sessionFactory("hibernate.cfg.xml").openSession();
-	}
+		Configuration c = new Configuration();
+
+		ClassLoader classLoader = null;
+		InputStream input = null;
+		try {
+
+			classLoader = Thread.currentThread().getContextClassLoader();
+			input = classLoader.getResourceAsStream("env.properties");
+			Properties prop = new Properties();
+			prop.load(input);
+
+			// load a properties file
+			prop.load(input);
+			c.setProperty("hibernate.connection.url", prop.getProperty("HIB_URL"))
+			 .setProperty("hibernate.connection.username", prop.getProperty("HIB_USERNAME"))
+			 .setProperty("hibernate.connection.password", prop.getProperty("HIB_PASSWORD"))
+		     .configure(filename);
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+   		ServiceRegistry sr = new StandardServiceRegistryBuilder().applySettings(c.getProperties()).build();
+        return c.buildSessionFactory(sr);
+    }
+
+    public static Session getSession() {
+        return sessionFactory("hibernate.cfg.xml").openSession();
+    }
 }
