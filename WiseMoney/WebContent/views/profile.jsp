2574d6e5f18336fe7a75dbdb5cc4a9430c7cc4bd
Bishwo Gurung <bishwo.gurung3@gmail.com>
2017-08-05 02:57:20 -0400

    Pass user session data from spring controller to jsp

--- WiseMoney/WebContent/views/profile.jsp
+++ WiseMoney/WebContent/views/profile.jsp
@@ -1,13 +1,48 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<html ng-app="wiseMoneyApp">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Profile Page</title>
+<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.8/angular.js"></script>
+<script
+	src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular-route.js"
+	type="text/javascript"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
 <body>
-	Hello there, I am logged in!
-
+<!-- <script type="text/javascript" src="js/StockCtrl.js"></script>
+ -->
+<%-- <% User user = (loginbean) req.getAttribute("user");
+ --%>	
+<!-- <div ng-controller="StockCtrl">
+	Hello there, I am logged in as {{stockData}}
+</div>
+	<h2>List of Stocks</h2>
+	<a class="btn btn-primary btn-md" href="#/">Stock List</a>
+ -->	
+	${userData.firstName}
+	${userData.lastName}
+	${userData.email}
+		
+		<a class="btn btn-primary btn-md" href="#/test">test</a>
+		<a class="btn btn-primary btn-md" href="#/stock">stock</a>
+	
+	
+	
+<div ng-view></div>
 </body>
+<script src="js/app.js"></script>
+<script src="js/TestCtrl.js"></script>
+<script src="js/StockCtrl.js"></script>
+
 </html>
--- WiseMoney/src/main/java/com/wisemoney/controller/UserController.java
+++ WiseMoney/src/main/java/com/wisemoney/controller/UserController.java
@@ -1,30 +1,31 @@
 package com.wisemoney.controller;
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wisemoney.dao.UserDao;
 import com.wisemoney.dao.UserDaoImpl;
+import com.wisemoney.domain.User;
 
 @Controller
 public class UserController {
 	
 	@RequestMapping(value="/register", method=RequestMethod.GET)
-	public String show() {
+	public String showRegistrationPage() {
 		return "register";
 	}
-	
-	
-//	@RequestMapping(value="/register", method=RequestMethod.GET)
-//	public ModelAndView showRegistrationPage(HttpServletRequest request,
-//			HttpServletResponse response) {
-////		ModelAndView model = new ModelAndView("register");
-//		  
-//		return new ModelAndView("register", "registerTrader", new User());
-//		
-//	}
-	
+
 	@RequestMapping(value="/register", method=RequestMethod.POST)
 	public String saveUser(HttpServletRequest req, HttpServletResponse resp) {
 		
@@ -38,8 +39,6 @@ public class UserController {
 		ud.register(username, firstName, lastName, password, email);
 		
 		return "login";
-		
-		
 	}
 	
 	@RequestMapping(value="/login", method=RequestMethod.GET)
@@ -48,40 +47,37 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public String loginUser(HttpServletRequest req, HttpServletResponse resp) {
-		
+	public String loginUser(HttpSession session, HttpServletRequest req, HttpServletResponse resp, Model m) {
 		String username = req.getParameter("username");
 		String password = req.getParameter("password");
 		
 		UserDao ud = new UserDaoImpl();
-		if ((Integer) ud.login(username, password) instanceof Integer) {
-			return "profile";
+		if (ud.login(username, password)!=null) {
+			User user = ud.login(username, password);
+			session.setAttribute("user", user);	
+			return "redirect:profile";
 		} else {
 			return "login";
 		}
-//		ud.register(username, firstName, lastName, password, email);
-		
-//		return "login";
-		
 		
 	}
 	
-//	@RequestMapping(value="/login", method=RequestMethod.POST)
-//	public String loginUser(@RequestParam("username") String username,
-//						@RequestParam("password") String password,
-//						
-//			@Valid @ModelAttribute("loginTrader") User user,
-//			BindingResult result, ModelMap model) {
-//		if (result.hasErrors()) {
-//			return "error";
-//		}
-//		
-//		UserDao ud = new UserDaoImpl();
-//		if ((ud.login(username, password))==0) {
-//			return "login";
-//		}
-//		else {
-//			return "profile";
-//		}
-//	}
+	@RequestMapping(value="/logout", method=RequestMethod.GET)
+	public void logoutUser(HttpSession session, HttpServletRequest req, HttpServletResponse resp) {
+		session = req.getSession();
+		session.invalidate();
+		try {
+			resp.sendRedirect("login");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@RequestMapping(value="/profile", method=RequestMethod.GET)
+	public String showUserProfilePage(HttpSession session, HttpServletRequest req) {
+		User user = (User) session.getAttribute("user");		
+		session.setAttribute("userData", user);
+		return "profile";
+	}
+	
 }
