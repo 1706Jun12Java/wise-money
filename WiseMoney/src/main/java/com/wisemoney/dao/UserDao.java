814f8561d15c23e8029495211a3f00e3e1063376
Bishwo Gurung <bishwo.gurung3@gmail.com>
2017-08-06 14:01:31 -0400

    Update DAOs for test cases

--- WiseMoney/src/main/java/com/wisemoney/dao/UserDao.java
+++ WiseMoney/src/main/java/com/wisemoney/dao/UserDao.java
@@ -6,10 +6,9 @@ import com.wisemoney.domain.User;
 
 public interface UserDao {
 	
-	public User getUser(int userId); //get details of a specific user
-	public int login(String username, String password);
-//	public void logout();
-	public List<User> getUserList(); //manager can view all employees
+//	public User getUser(int userId); //get details of a specific user
+	public User login(String username, String password);
+//	public void logout(); //done in UserController because it's simply session invalidation
 	public void register(String username, String firstname, String lastname, String password, String email);
 
 }
--- WiseMoney/src/main/java/com/wisemoney/dao/UserDaoImpl.java
+++ WiseMoney/src/main/java/com/wisemoney/dao/UserDaoImpl.java
@@ -3,10 +3,12 @@ package com.wisemoney.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Projections;
 
 import com.wisemoney.domain.Stock;
 import com.wisemoney.domain.User;
@@ -14,34 +16,35 @@ import com.wisemoney.domain.UserRole;
 import com.wisemoney.util.HibernateUtil;
 
 public class UserDaoImpl implements UserDao {
-
 	//get a user by id
+//	@Override
+//	public User getUser(int userId) {
+//		Session s = HibernateUtil.getSession();
+//		Query query = s.createQuery("select u.id, u.firstName, u.lastName, u.username, u.email, u.userRole from User u where u.id=:id");
+//		query.setInteger("id", userId);
+//		User user = (User) query.uniqueResult();
+//		return user;
+//	}
+
 	@Override
-	public User getUser(int userId) {
+	public User login(String username, String password) {
 		Session s = HibernateUtil.getSession();
-		Query query = s.createQuery("from User u where u.id=:id");
-		query.setInteger("id", userId);
+		Query query = s.createQuery("from User u where u.username=:username and u.password=:password");
+		query.setString("username", username);
+		query.setString("password", password);
 		User user = (User) query.uniqueResult();
+//		Criteria cr = s.createCriteria(User.class)
+		
 		return user;
 	}
 
-	@Override
-	public int login(String username, String password) {
-		Session s = HibernateUtil.getSession();
-		Query query = s.createQuery("select id from User u where u.username=:username and u.password=:password");
-		query.setString("username", username);
-		query.setString("password", password);
-		int userId = (int) query.uniqueResult();
-		return userId;
-	}
-
-	@Override
-	public List<User> getUserList() {
-		Session s = HibernateUtil.getSession();
-		List<User> users = new ArrayList<User>();
-		users = s.createQuery("from User").list();
-		return users;
-	}
+//	@Override
+//	public List<User> getUserList() {
+//		Session s = HibernateUtil.getSession();
+//		List<User> users = new ArrayList<User>();
+//		users = s.createQuery("from User").list();
+//		return users;
+//	}
 
 	@Override
 	public void register(String username, String firstname, String lastname, String password, String email) {
@@ -59,7 +62,6 @@ public class UserDaoImpl implements UserDao {
             User user = new User(firstname, lastname, username, password, email, userRole);
             s.save(user);
             tx.commit();
-            System.out.println("\n\n Details Added \n");
  
         } catch (HibernateException e) {
             System.out.println(e.getMessage());
